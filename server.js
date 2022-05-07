4bdf2c49849b7aab82e99951265e12ed3377208b
JodyBailey <jjeb2020@gmail.com>
2022-05-07 09:12:50 +0200

    Also made the table be made in server file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const newVisit = require("./routes/new_visit");
+const { createTable } = require("./src/index");
 const path = require("path");
 const app = express();
 const PORT = 3256;
@@ -11,6 +12,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use("/new_visit", newVisit);
+createTable();
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
